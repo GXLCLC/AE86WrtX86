fdbda095aa397b65e70962de63afc845f7e84724
GXLCLC <2193160597@qq.com>
2026-09-04 11:12:16 +0800

    Delete diy/mt798x directory

--- diy/mt798x/imm1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#=================================================
-# DaoDao's script
-#=================================================
-##添加自己的插件库
-sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
--- diy/mt798x/op1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#=================================================
-# DaoDao's script
-#=================================================
-##添加自己的插件库
-sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
